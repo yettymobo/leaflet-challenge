2086f68b43c4c13f684a7efcb5452841c8e9b6a4
Patricia Mobolade <pmob@live.com>
2020-12-09 20:31:50 -0600

    Troubleshooting map rendering issues

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -5,7 +5,7 @@ var myMap = L.map("mapid", {
   });
   
   // Add a tile layer
-  L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  var darkMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "streets-v11",
@@ -55,10 +55,11 @@ var myMap = L.map("mapid", {
           return mag * 3;
     }
 
-
-
-
-
-
     
+    
+     
+
+
+
+
 })
