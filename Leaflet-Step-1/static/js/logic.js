7ee13fe3262b16807ee8e29ca8283bf31dae3631
Patricia Mobolade <pmob@live.com>
2020-12-09 22:02:36 -0600

    Rewrting using example from class

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -16,14 +16,33 @@ var myMap = L.map("mapid", {
   
   darkMap.addTo(myMap);
   
-  
+  // Declare data variable
   var link = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson'
 
-  // Add a marker to the map for each earthquake
+  // Loop through the features array and create one circle for each location
+
   d3.json(link, function(data) {
       console.log(data);
-    function Color(depth) {
-        switch (true) {
+
+      data.forEach(function(feature){
+               
+        var depth = feature.geometry.coordinates[2];
+        var mag = feature.properties.mag
+        console.log(depth)
+         
+        L.circle([feature.geometry.coordinates[0], feature.geometry.coordinates[1]],{
+            fillOpacity: 0.75,
+            color: color,
+            fillColor: color,
+            
+    // Adjust radius
+         radius: mag * 2000
+        }).bindPopup("Location: " + feature.properties.place + "<hr>Magnitude: " + feature.properties.mag + "<br>Location: " + "<br> Depth: "+ feature.geometry.coordinates[2])
+            .addTo(myMap);   
+        
+        
+        var color = "";
+        switch (color) {
           case depth <=50:
             return "#bcd9ea";
           case depth <=100:
@@ -40,37 +59,69 @@ var myMap = L.map("mapid", {
               return "#0c3953";
           default:
             return "#e4f0f6";
-        }
-    }
+        }})
 
-    function Radius(mag) {
-        if (mag === 0) {
-          return 1;
-        }
-          return mag * 3;
-    }
+
+        
+        
+        
+
+
+        
+        // add circles to map
+        
+
+})
+
+//     function Color(depth) {
+//         switch (true) {
+//           case depth <=50:
+//             return "#bcd9ea";
+//           case depth <=100:
+//             return "#5ba4cf";
+//           case depth <=200:
+//             return "#298fca";
+//           case depth <=300:
+//             return "#0079bf";
+//           case depth <=400:
+//             return "#055a8c";
+//           case depth <=500:
+//               return "#094c72";
+//           case depth >500:
+//               return "#0c3953";
+//           default:
+//             return "#e4f0f6";
+//         }
+//     }
+
+//     function Radius(mag) {
+//         if (mag === 0) {
+//           return 1;
+//         }
+//           return mag * 3;
+//     }
     
-    function Style(feature) {
-        return {
-          opacity: 1,
-          fillOpacity: 1,
-          fillColor: Color(feature.geometry.coordinates[2]),
-          color: "#000000",
-          radius: Radius(feature.properties.mag),
-          stroke: true,
-          weight: 0.5
-        }
-    }
+//     function Style(feature) {
+//         return {
+//           opacity: 1,
+//           fillOpacity: 1,
+//           fillColor: Color(feature.geometry.coordinates[2]),
+//           color: "#000000",
+//           radius: Radius(feature.properties.mag),
+//           stroke: true,
+//           weight: 0.5
+//         }
+//     }
     
 
-    L.geoJson(data, {
-        pointToLayer: function(feature, latlng) {
-          return L.circleMarker(latlng);
-        },
-        style: Style,
-        onEachFeature: function(feature, layer) {
-          layer.bindPopup("Location: " + feature.properties.place + "<hr>Magnitude: " + feature.properties.mag + "<br>Location: " + "<br> Depth: "+ feature.geometry.coordinates[2]);
-        }
+//     L.geoJson(data, {
+//         pointToLayer: function(feature, latlng) {
+//           return L.circleMarker(latlng);
+//         },
+//         style: Style,
+//         onEachFeature: function(feature, layer) {
+//           layer.bindPopup("Location: " + feature.properties.place + "<hr>Magnitude: " + feature.properties.mag + "<br>Location: " + "<br> Depth: "+ feature.geometry.coordinates[2]);
+//         }
     
-      }).addTo(myMap);
-  })
+//       }).addTo(myMap);
+//   })
